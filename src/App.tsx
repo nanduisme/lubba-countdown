a5d28c176f567778d1936959409fdbccf2523bd2
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2022-05-13 14:17:45 +0530

    added slideshow

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,43 @@ const App = () => {
   const setDate = new Date("9/9/2020");
   const today = new Date();
   const diff = Math.abs(setDate.getTime() - today.getTime());
-  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-  const months = 12 - Math.abs(today.getMonth() - setDate.getMonth());
-  const days = today.getDate()
+  let years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+  let months = 12 - Math.abs(today.getMonth() - setDate.getMonth());
 
-  const addS = (arg: number) => {
-    return arg > 1 ? "s" : "";
+  const asString = (arg: number, name: string) => {
+    return arg ? `${arg} ${name}${arg > 1 ? "s" : ""}` : "";
+  };
+
+  const monthDays = [
+    31,
+    today.getFullYear() % 4 ? 28 : 29,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  let days = today.getDate() - setDate.getDate();
+  if (days < 0) {
+    months--;
+    if (months < 0) {
+      years--;
+      months = 0;
+    }
+
+    days += monthDays[today.getMonth()];
   }
 
   return (
-    <div>{`${years} Year${addS(years)} ${months} Month${addS(months)} ${days} Day${addS(days)}`}</div>
+    <div>
+      {`${asString(years, "Year")} ${asString(months, "Month")} ${asString(days, "Day")}`}
+    </div>
   );
 };
 
